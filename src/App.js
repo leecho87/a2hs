2687b6726ff0a8f971bb934f171c4b026068389a
changho <leecho87@naver.com>
2023-07-05 09:38:06 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const useA2HS = () => {
+  /**
+   * prompt가 실행될 수 있는 환경인 경우에만 모달창을 나타내기 위해
+   * 변경 시 리렌더링을 발생시키기 위해서 useRef가 아닌 useState를 사용하였습니다.
+   */
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  useEffect(() => {
+    const handler = (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    };
+    // beforeinstallprompt에 이벤트 핸들러를 등록합니다.
+    window.addEventListener('beforeinstallprompt', handler);
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handler);
+    };
+  }, []);
+
+  const installApp = () => {
+    // 설치 메서드 실행
+    deferredPrompt?.prompt();
+    deferredPrompt?.userChoice.then(choiceResult => {
+      clearPrompt();
+    });
+  };
+
+  const clearPrompt = () => {
+    setDeferredPrompt(null);
+  };
+
+  return { deferredPrompt, installApp, clearPrompt };
 }
 
-export default App;
+const App = () => {
+  const { deferredPrompt, installApp, clearPrompt } = useA2HS();
+
+  return deferredPrompt ? (
+    <div>
+      <button onClick={clearPrompt}>취소</button>
+      <button onClick={installApp}>홈 화면에 추가</button>
+    </div>
+  ) : (
+    <>
+      {JSON.stringify(deferredPrompt)}
+    </>
+  )
+}
+
+export default App
