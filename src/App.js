ba2bf52420ca8c6c6b290c0f5d9dbb5dd5f18b20
changho <leecho87@naver.com>
2023-07-05 09:46:39 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useState, useEffect } from "react";
 
 const useA2HS = () => {
   /**
@@ -14,16 +13,16 @@ const useA2HS = () => {
       setDeferredPrompt(e);
     };
     // beforeinstallprompt에 이벤트 핸들러를 등록합니다.
-    window.addEventListener('beforeinstallprompt', handler);
+    window.addEventListener("beforeinstallprompt", handler);
     return () => {
-      window.removeEventListener('beforeinstallprompt', handler);
+      window.removeEventListener("beforeinstallprompt", handler);
     };
   }, []);
 
   const installApp = () => {
     // 설치 메서드 실행
     deferredPrompt?.prompt();
-    deferredPrompt?.userChoice.then(choiceResult => {
+    deferredPrompt?.userChoice.then((choiceResult) => {
       clearPrompt();
     });
   };
@@ -33,21 +32,24 @@ const useA2HS = () => {
   };
 
   return { deferredPrompt, installApp, clearPrompt };
-}
+};
 
 const App = () => {
   const { deferredPrompt, installApp, clearPrompt } = useA2HS();
 
-  return deferredPrompt ? (
-    <div>
-      <button onClick={clearPrompt}>취소</button>
-      <button onClick={installApp}>홈 화면에 추가</button>
-    </div>
-  ) : (
+  return (
     <>
-      {JSON.stringify(deferredPrompt)}
+      <h1>load</h1>
+      {deferredPrompt ? (
+        <div>
+          <button onClick={clearPrompt}>취소</button>
+          <button onClick={installApp}>홈 화면에 추가</button>
+        </div>
+      ) : (
+        <>{JSON.stringify(deferredPrompt)}</>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
